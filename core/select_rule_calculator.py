8ec6d45c899c318ebda64eacf33be3d96e1036c5
Xingye-Dujing <1933679780@qq.com>
2026-01-02 14:53:29 +0800

    修改

--- core/select_rule_calculator.py
+++ core/select_rule_calculator.py
@@ -1,5 +1,5 @@
 from typing import Tuple
-from IPython.display import Math, display
+# from IPython.display import Math, display
 from sympy import Expr, latex
 
 from utils import Context, Operation, RuleContext
@@ -19,7 +19,8 @@ class SelectRuleCalculator(BaseCalculator):
             if not self._check_rule_is_can_apply(rule):
                 applicable_rules_list.remove(rule)
 
-        display(Math(latex(expr)))
+        # display(Math(latex(expr)))
+        print(expr)
         print("以下规则可应用：")
         for i, rule in enumerate(applicable_rules_list):
             print(f"{i+1}. {rule.__name__}")
@@ -28,7 +29,8 @@ class SelectRuleCalculator(BaseCalculator):
         result = rule(expr, rule_context)
 
         if result:
-            display(Math(latex(result[0])))
+            # display(Math(latex(result[0])))
+            print(latex(result[0]))
             return result
 
         print("此规则无法应用，请重新选择.")
